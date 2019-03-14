d19672c08cb5ff94abfc643cc6982de66affb133
Wu <mk.danielwu@gmail.com>
2019-03-14 13:24:14 -0700

    export resluts to files for easy of analysis and plotting

--- config.py
+++ config.py
@@ -1,46 +1,55 @@
+import datetime
 from constant import *
 
 
-class ConfigFixedDose:
-
+class ConfigCommon:
     def __init__(self):
-        self.algo_name = "FixedDose"
-
         # output config
-        self.output_path = "results/{}/".format(self.algo_name)
+        self.output_path = "results/{}/".format(datetime.datetime.now().strftime("%Y%m%d_%H%M%S"))
         self.log_path = self.output_path + "log.txt"
         self.regret_plot_output = self.output_path + "regret.png"
-        self.payoff_plot_output = None
-        self.cfinterval_plot_output = None
+        self.payoff_plot_output = self.output_path + "payoff.png"
+        self.cfinterval_plot_output = self.output_path + "cfinterval.png"
+
+    def get_regret_filename(self, model, is_training):
+        s = "training" if is_training else "testing"
+        return f"{self.output_path}{s}_regret_{model}.csv"
+
+    def get_mistake_filename(self, model, is_training):
+        s = "training" if is_training else "testing"
+        return f"{self.output_path}{s}_mistake_{model}.csv"
+
+    def get_payoff_filename(self, model, is_training):
+        s = "training" if is_training else "testing"
+        return f"{self.output_path}{s}_payoff_{model}.csv"
+
+    def get_conf_interval_filename(self, model, is_training):
+        s = "training" if is_training else "testing"
+        return f"{self.output_path}{s}_conf_interval_{model}.csv"
+
+
+class ConfigFixedDose(ConfigCommon):
+
+    def __init__(self):
+        super().__init__()
+        self.algo_name = "FixedDose"
 
         # parameters for the model
         self.fixed_dose = DOSE_MED
 
 
-class ConfigClinicalDose:
+class ConfigClinicalDose(ConfigCommon):
 
     def __init__(self):
+        super().__init__()
         self.algo_name = "ClinicalDose"
 
-        # output config
-        self.output_path = "results/{}/".format(self.algo_name)
-        self.log_path = self.output_path + "log.txt"
-        self.regret_plot_output = self.output_path + "regret.png"
-        self.payoff_plot_output = None
-        self.cfinterval_plot_output = None
 
-
-class ConfigLinUCBDisjoint:
+class ConfigLinUCBDisjoint(ConfigCommon):
 
     def __init__(self):
+        super().__init__()
         self.algo_name = "LinUCBDisjoint"
-
-        # output config
-        self.output_path = "results/{}/".format(self.algo_name)
-        self.log_path = self.output_path + "log.txt"
-        self.regret_plot_output = self.output_path + "regret.png"
-        self.payoff_plot_output = self.output_path + "payoff.png"
-        self.cfinterval_plot_output = self.output_path + "cfinterval.png"
 
         # parameters for the model
         self.actions = [DOSE_LOW, DOSE_MED, DOSE_HIGH]
