7a86dfd232c3614a35ffec62c8027f3f9310153d
Wu <mk.danielwu@gmail.com>
2019-03-17 09:22:12 -0700

    remove get_logger

--- util.py
+++ util.py
@@ -48,20 +48,6 @@ def get_int(s):
     return value
 
 
-# def get_logger(filename):
-#     """
-#     Return a logger instance to a file
-#     """
-#     logger = logging.getLogger('logger')
-#     logger.setLevel(logging.DEBUG)
-#     logging.basicConfig(format='%(message)s', level=logging.DEBUG)
-#     handler = logging.FileHandler(filename)
-#     handler.setLevel(logging.DEBUG)
-#     handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s: %(message)s'))
-#     logging.getLogger().addHandler(handler)
-#     return logger
-#
-
 def export_stats_list(stats_list, filename):
     """
     Export a stats list to a file
